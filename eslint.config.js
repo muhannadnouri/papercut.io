9d0255b0b319734e281cf9819098ec273d8c75a2
Muhannad Nouri <muhannadnouri@users.noreply.github.com>
2026-07-31 19:23:55 -0600

    linting(config): add ignore path for python sidecar venv

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'src-tauri/target/**']),
+  globalIgnores(['dist', '.venv-silma/**', 'src-tauri/target/**']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
